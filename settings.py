57a98891c8763ffac1047227eb3425bf69390b2a
Luong-Minh-Quang <lmquang2001@gmail.com>
2023-04-19 20:26:23 +0800

    .

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ MM_SIZE = PAGE_SIZE * 200
 DATA_DIR = "./processed_data"
 if PAGE_SIZE % 8 != 0:
     raise ValueError("Disk page size should be a multiple of 8")
+
+if __name__ == "__main__":
+    print(int("M"))
